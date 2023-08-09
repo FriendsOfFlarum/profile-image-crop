84e7dceed2bff8212a2b3fdd54e6630fcb8321e8
David Sevilla Martin <me@dsevilla.dev>
2023-08-08 23:32:34 -0400

    Fix `image-blob-reduce` issue caused by webpack & terser optimizations

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path');
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(
   require('flarum-webpack-config')(),
   {
     output: {
       chunkFilename: 'chunk~[name].js',
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              evaluate: false, // fix for image-blob-reduce
+              passes: 2, // default value
+            },
+          },
+        })
+      ],
+      sideEffects: false, // fix for image-blob-reduce
     },
     plugins: [
       new CleanWebpackPlugin({
